bc4a75f1567391d2a1182adab96215a27138bc46
James Withers <james@auderecommerce.com>
2021-08-20 11:15:35 +0100

    2.0.0

--- js/audere.js
+++ js/audere.js
@@ -1,3 +1,34 @@
+var responsiveHeader = {
+    onResize: function () {
+        $(window).resize(function () {
+            responsiveHeader.checkWidth();
+        });
+    },
+    checkWidth: function () {
+        if (window.innerWidth >= 1240) {
+            responsiveHeader.desktop();
+        } else {
+            responsiveHeader.mobile();
+        }
+    },
+    desktop: function () {
+        $('#main_navigation').appendTo('.page-header .page-main').find('li.button > a').addClass('action primary');
+    },
+    mobile: function () {
+        $('#main_navigation').appendTo('#mobile_sidebar').find('li.button > a').removeClass('action primary');
+    },
+    init: function () {
+        this.onResize();
+        this.checkWidth();
+    }
+};
+
+responsiveHeader.init();
+
+$('#menu_icon').click(function() {
+    $('body').toggleClass('mobile-sidebar-visible');
+});
+
 var fixedHeader = {
     onScroll: function() {
         $(window).scroll(function() {
@@ -25,44 +56,22 @@ var fixedHeader = {
 
 fixedHeader.init();
 
-var responsiveHeader = {
-    onResize: function () {
-        $(window).resize(function () {
-            responsiveHeader.checkWidth();
-        });
-    },
-    checkWidth: function () {
-        if (window.innerWidth >= 1024) {
-            responsiveHeader.desktop();
-        } else {
-            responsiveHeader.mobile();
-        }
-    },
-    desktop: function () {
-        $('#main_navigation').appendTo('.page-header .page-main');
-        $('#main_navigation li.contact > a').addClass('action primary');
-    },
-    mobile: function () {
-        $('#main_navigation').appendTo('#mobile_sidebar');
-        $('#main_navigation li.contact > a').removeClass('action primary');
-    },
-    init: function () {
-        this.onResize();
-        this.checkWidth();
-    }
-};
-
-responsiveHeader.init();
-
-$('#menu_icon').click(function() {
-    $('body').toggleClass('mobile-sidebar-visible');
-});
-
 $('#main_navigation a, a.anchor-link, footer ul.navigation a').click(function (e) {
     e.preventDefault();
     $('body').removeClass('mobile-sidebar-visible');
     var target = $(this).attr('href');
+
+    var headerSize;
+
+    if (window.innerWidth >= 1240) {
+        headerSize = 90;
+    } else if (window.innerWidth >= 768) {
+        headerSize = 84;
+    } else {
+        headerSize = 73;
+    }
+
     $('html, body').animate({
-        scrollTop: $(target).offset().top - 60
+        scrollTop: $(target).offset().top - headerSize
     }, 300);
 });
